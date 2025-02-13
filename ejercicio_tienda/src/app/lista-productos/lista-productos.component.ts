381f354840ae663dd8d46cbc19cda3ab14eca6c5
David MTZD <davidmtzd21512@gmail.com>
2025-02-12 18:07:09 -0600

    Agregar funcionalidad para añadir nuevos productos en la lista de productos

--- ejercicio_tienda/src/app/lista-productos/lista-productos.component.ts
+++ ejercicio_tienda/src/app/lista-productos/lista-productos.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { ProductoComponent } from "./producto/producto.component";
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-lista-productos',
-  imports: [ProductoComponent],
+  imports: [ FormsModule],
   templateUrl: './lista-productos.component.html',
   styleUrl: './lista-productos.component.css'
 })
 export class ListaProductosComponent {
-
+  nombreProducto: string="";
+  precioProducto: number=0;
    titulo = "Lista de productos";
-}
+    productos = [
+      {descripcion: "Producto 1", precio: 100},
+      {descripcion: "Producto 2", precio: 200},
+      {descripcion: "Producto 3", precio: 300}
+    ];
+
+    agregarProducto():void {
+     this.productos.push({  descripcion: this.nombreProducto, precio: this.precioProducto });
+    }
+  }
