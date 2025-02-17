5f0360c3d87c260b1cbf096eeaf3e3f99ac9c05e
David MTZD <davidmtzd21512@gmail.com>
2025-02-16 19:29:33 -0600

    Agregar componente FormularioProducto y refactorizar la lista de productos para utilizar la clase Producto

--- ejercicio_tienda/src/app/lista-productos/lista-productos.component.ts
+++ ejercicio_tienda/src/app/lista-productos/lista-productos.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { ProductoComponent } from "./producto/producto.component";
 import { FormsModule } from '@angular/forms';
+import { FormularioProductoComponent } from "./formulario-producto/formulario-producto.component";
+import { Producto } from './producto/producto.module';
 
 
 @Component({
   selector: 'app-lista-productos',
-  imports: [ FormsModule],
+  imports: [FormsModule, FormularioProductoComponent],
   templateUrl: './lista-productos.component.html',
   styleUrl: './lista-productos.component.css'
 })
@@ -13,13 +14,13 @@ export class ListaProductosComponent {
   nombreProducto: string="";
   precioProducto: number=0;
    titulo = "Lista de productos";
-    productos = [
-      {descripcion: "Producto 1", precio: 100},
-      {descripcion: "Producto 2", precio: 200},
-      {descripcion: "Producto 3", precio: 300}
-    ];
+  productos: Producto[] = [
+new Producto('Pantalón', 130.0),
+new Producto('Camisa', 80.0),
+new Producto('Playera', 50.0),
+];
 
-    agregarProducto():void {
-     this.productos.push({  descripcion: this.nombreProducto, precio: this.precioProducto });
+    agregarProducto(producto:Producto) {
+     this.productos.push(new Producto(producto.descripcion,producto.precio));
     }
   }
